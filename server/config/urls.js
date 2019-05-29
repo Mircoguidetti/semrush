f450d16e32bd9de9e98f8a81d7e04d26833e61a4
Mirco Guidetti <mirco@guidetti.co>
2019-05-29 14:11:39 +0100

    Modified url query

--- server/config/urls.js
+++ server/config/urls.js
@@ -3,10 +3,6 @@ const { semRushApiKey } = require('./keys');
 module.exports = (typeApi, domain, database) => {
     switch(typeApi) {
         case 'organic-keywords':
-            return `https://api.semrush.com/?type=domain_organic&
-                    key=${semRushApiKey}&display_limit=30&
-                    display_filter=-%7CPo%7CGt%7C10&export_columns=Ph,Nq,Po&
-                    domain=${domain}&display_sort=po_asc&database=${database}&
-                    display_offset=4`
+            return `https://api.semrush.com/?type=domain_organic&key=${semRushApiKey}&display_limit=30&display_filter=-%7CPo%7CGt%7C10&export_columns=Ph,Nq,Po&domain=${domain}&display_sort=po_asc&database=${database}&display_offset=4`
     }
 }
